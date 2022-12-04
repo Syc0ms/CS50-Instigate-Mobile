130562638b58f55a2b3934194eb34f00482c47f2
Sycoms <babaxanyanbenjamin@gmail.com>
2022-12-04 16:14:49 +0400

    last

--- stack.cpp
+++ stack.cpp
@@ -15,22 +15,45 @@ StackNode* newNode(int data) {
 	return stack;
 }
 
-int isEmpty(StackNode* root) {
-	return !root;
+int isEmpty(StackNode* main) {
+	return !main;
 }
 
-void push(StackNode** root, int data) {
+void push(StackNode** main, int data) {
     StackNode* stackNode = newNode(data);
-    stackNode->next = *root;
-    *root = stackNode;
+    stackNode->next = *main;
+    *main = stackNode;
     cout << data << " - this number pushed in stack \n";
 }
-int main() {
-    StackNode* root = NULL;
 
-    push(&root, 10);
-    push(&root, 20);
-    push(&root, 30);
+int pop(StackNode** main) {
+        if(isEmpty(*main))
+                return INT_MIN;
+        StackNode* temp = *main;
+        *main = (*main)->next;
+        int popped = temp->data;
+        free(temp);
 
+        return popped;
+        
+}
+
+int peek(StackNode* main) {
+    if (isEmpty(main))
+        return INT_MIN;
+    return main->data;
+}
+
+int initStack(){ 
+    StackNode* main = NULL;
+  
+    push(&main, 10);
+    push(&main, 20);
+    push(&main, 30);
+    cout << pop(&main) << " - this number is popped from stack\n";
+    cout << "Top element is " << peek(main) << endl;
     return 0;
+}
+int main() {
+        initStack();
 }
